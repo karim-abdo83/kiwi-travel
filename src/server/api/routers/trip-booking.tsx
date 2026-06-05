8696aa6b12688aba0e03e9fac32ad31ce2d26c86
karim-abdo83 <kiwitraveleg@gmail.com>
2026-06-06 01:08:28 +0300

    Update trip-booking.tsx
    telegram bot change

--- src/server/api/routers/trip-booking.tsx
+++ src/server/api/routers/trip-booking.tsx
@@ -196,8 +196,21 @@ export const tripBookingRouter = createTRPCRouter({
       const tripLink = `${env.NEXT_PUBLIC_APP_URL}/trips/${trip.slug}`;
 
       await sendTelegramNotification(
-        `🧾 <b>Новая бронь</b>\nПользователь: ${input.email}\nТелефон: ${input.phone}\nТур: ${input.tripId}\nДата: ${format(input.date, "yyyy-MM-dd")}\n<a href="${bookingLink}">Открыть в админке</a>\n\n<a href="${tripLink}">${trip.titleRu}</a>`
-      );
+🧾 <b>Новая бронь</b>
+🆔 <b>Trip ID:</b> ${input.tripId}
+👤 <b>Клиент:</b> ${input.name || user.firstName || "Guest"}
+📧 <b>Email:</b> ${user.emailAddresses[0]?.emailAddress || "Не указан"}
+📞 <b>Телефон:</b> ${input.phone}
+🎫 <b>Тур:</b> ${trip.titleRu || trip.titleEn}
+📅 <b>Дата:</b> ${format(input.date, "yyyy-MM-dd")}
+👨 <b>Взрослые:</b> ${input.adultsCount}
+👧 <b>Дети:</b> ${input.childrenCount}
+👶 <b>Младенцы:</b> ${input.infantsCount}
+💰 <b>Сумма:</b> $${emailTotal}
+💳 <b>Оплата:</b> Наличные
+🔗 <a href="${bookingLink}">Открыть в админке</a>
+🌍 <a href="${tripLink}">Ссылка на тур</a>`
+);
       const tEmail = await getTranslations("General.bookingEmail.new");
 
       // Calculate totals for email
